0a768e995ba06d304970ba72df23691130912033
SANHOKIM <awrko@naver.com>
2024-01-10 16:25:03 +0900

    Content.js.navi.json.map.el.d1text 리턴(화면에표시) / Content.js의useParams의 {languagess} 와 App.js의 path=/:languagess , 그리고  react-router-dom Link {/}는 path=변수에 할당된다.미칀까다로워

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import navi from "./data/navi.json";
+// import navi from "./data/navi.json";
 import Contents from "./components/Contents";
 
 function App() {
-  /*  navi.json 파일로 넘기기 
+  // navi.json 파일로 넘기기
   const navi = [
     {
-      d1text: "네이버",
+      d1text: "한국어",
       cls: "naver",
-      href: "naver",
+      href: "kr",
     },
     {
-      d1text: "다음",
+      d1text: "영어",
       cls: "daum",
-      href: "daum",
+      href: "en",
     },
-  ]; */
+  ];
   return (
     <div className="App">
       <header>
-        {navi.map((el, idx) => {
+        {/* {navi.map((el) => {
           return <Link to={`/kr/${el.href}`}>{el.d1text}</Link>;
+        })} */}
+
+        {navi.map((el, idx) => {
+          return (
+            <Link to={`/${el.href}`} key={`navi${idx}`}>
+              {el.d1text}
+            </Link>
+          );
         })}
       </header>
       <section>
         <Routes>
           <Route
-            path="/kr/:languagess"
+            path="/:languagess"
             element={
               <Contents>
-                <p>Contents의 p태그 </p>
+                <p>App.js.Contents.p태그 </p>
               </Contents>
             }
           ></Route>
@@ -39,5 +47,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
--- src/components/Contents.js
+++ src/components/Contents.js
@@ -3,13 +3,21 @@ import contentnavi from "../data/navi.json";
 
 function Contents({ children }) {
   const { languagess } = useParams();
-
   return (
     <section>
-      {languagess}
+      {languagess} 사이트 입니다.
       {children}
+      {contentnavi[languagess].navi.map((el) => {
+        return el.d1text;
+      })}
     </section>
   );
+  // return (
+  //   <section>
+  //     {languagess}
+  //     {children}
+  //   </section>
+  // );
 }
 
 export default Contents;
